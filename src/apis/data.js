129be4560527c1c326c99d82f62d0ffeb3775a73
aswini <gvc.ashwini@gmail.com>
2021-07-22 11:09:29 +0530

    solving cors issue

--- src/apis/data.js
+++ src/apis/data.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://immense-plateau-20755.herokuapp.com/",
+  baseURL:
+    "https://cors-anywhere.herokuapp.com/https//immense-plateau-20755.herokuapp.com/",
 });
