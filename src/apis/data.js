9a0407690413ced8857d28c241db72fa31372a01
aswini <gvc.ashwini@gmail.com>
2021-07-26 12:05:22 +0530

    cahnges to styles

--- src/apis/data.js
+++ src/apis/data.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export default axios.create({
   baseURL:
-    "https://cors-anywhere.herokuapp.com/https//immense-plateau-20755.herokuapp.com/",
+    "https://cors-anywhere-herokuapp.com/https://immense-plateau-20755.herokuapp.com/",
 });
